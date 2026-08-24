f7a1c12c5a3552739a8f8ff3566482337bf79572
ZAARD <143908070+figueredo56@users.noreply.github.com>
2026-08-24 13:07:37 -0500

    Refactor ZARD contract to use OpenZeppelin ERC20
    Refactored ZARD contract to inherit from ERC20 and Ownable, simplifying token implementation.

--- ZARD.sol
+++ ZARD.sol
@@ -1,47 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-contract ZARD {
-    string public name = "ZARD";
-        string public symbol = "ZARD";
-            uint8 public decimals = 18;
-                uint256 public totalSupply;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-                    mapping(address => uint256) public balanceOf;
-                        mapping(address => mapping(address => uint256)) public allowance;
-
-                            event Transfer(address indexed from, address indexed to, uint256 value);
-                                event Approval(address indexed owner, address indexed spender, uint256 value);
-
-                                    constructor() {
-                                            totalSupply = 200 * 10**decimals;
-                                                    balanceOf[msg.sender] = totalSupply;
-                                                            emit Transfer(address(0), msg.sender, totalSupply);
-                                                                }
-
-                                                                    function transfer(address to, uint256 value) public returns (bool) {
-                                                                            require(balanceOf[msg.sender] >= value, "Insufficient balance");
-                                                                                    balanceOf[msg.sender] -= value;
-                                                                                            balanceOf[to] += value;
-                                                                                                    emit Transfer(msg.sender, to, value);
-                                                                                                            return true;
-                                                                                                                }
-
-                                                                                                                    function approve(address spender, uint256 value) public returns (bool) {
-                                                                                                                            allowance[msg.sender][spender] = value;
-                                                                                                                                    emit Approval(msg.sender, spender, value);
-                                                                                                                                            return true;
-                                                                                                                                                }
-
-                                                                                                                                                    function transferFrom(address from, address to, uint256 value) public returns (bool) {
-                                                                                                                                                            require(balanceOf[from] >= value, "Insufficient balance");
-                                                                                                                                                                    require(allowance[from][msg.sender] >= value, "Allowance exceeded");
-                                                                                                                                                                            
-                                                                                                                                                                                    allowance[from][msg.sender] -= value;
-                                                                                                                                                                                            balanceOf[from] -= value;
-                                                                                                                                                                                                    balanceOf[to] += value;
-                                                                                                                                                                                                            
-                                                                                                                                                                                                                    emit Transfer(from, to, value);
-                                                                                                                                                                                                                            return true;
-                                                                                                                                                                                                                                }
-                                                                                                                                                                                                                                }
+contract ZARD is ERC20, Ownable {
+    constructor() ERC20("ZARD", "ZARD") Ownable(msg.sender) {
+            // 10,000 tokens exactos con 18 decimales
+                    _mint(msg.sender, 10000 * 10 ** decimals());
+                        }
+                        }
